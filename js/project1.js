630aa3ee276929f33f009b909ea140707cd7ed71
Neeraj Kumar <56425717+neerajdaksh@users.noreply.github.com>
2024-01-11 21:34:05 +0530

    color switcher project

---
+++ js/project1.js
@@ -0,0 +1,20 @@
+const buttons = document.querySelectorAll(".button");
+const container = document.querySelector(".color-switcher");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        if (e.target.id === "gray") {
+            container.style.backgroundColor = "gray";
+        }
+        if (e.target.id === "pink") {
+            container.style.backgroundColor = "pink";
+        }
+        if (e.target.id === "blue") {
+            container.style.backgroundColor = "blue";
+        }
+        if (e.target.id === "yellow") {
+            container.style.backgroundColor = "yello";
+        }
+
+    })
+})
